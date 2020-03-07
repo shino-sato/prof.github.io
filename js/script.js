b3adecd196790ba8e63e7673472f1648626d1a65
shino-satohi <61077610+shino-satohi@users.noreply.github.com>
2020-03-07 18:43:04 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-$(function() {
-// アコーディオン
-$('.school').click(function() {
-    var $answer = $(this).find('.detasills');
-    if($answer.hasClass('open')) {
-      $answer.removeClass('open');
-      // slideUpメソッドを用いて、$answerを隠してください
-      $answer.slideUp();
+jQuery(document).ready(function(){
+  $('.school').click(function() {
+      var $answer = $(this).find('.detasills');
+      if($answer.hasClass('open')) {
+        $answer.removeClass('open');
+        // slideUpメソッドを用いて、$answerを隠してください
+        $answer.slideUp();
 
-      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
-      $(this).find("span").text("+");
+        // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+        $(this).find("span").text("+");
 
-    } else {
-      $answer.addClass('open');
-      // slideDownメソッドを用いて、$answerを表示してください
-      $answer.slideDown();
+      } else {
+        $answer.addClass('open');
+        // slideDownメソッドを用いて、$answerを表示してください
+        $answer.slideDown();
 
-      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
-      $(this).find("span").text("-");
-    }
-  });
-});
+        // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+        $(this).find("span").text("-");
+      }
+    });
+ });
+
